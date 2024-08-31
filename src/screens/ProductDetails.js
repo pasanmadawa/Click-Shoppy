8259b5aeae6b3a32658fa74ff37e8b04f50eedfa
pasanmadawa <126885242+pasanmadawa@users.noreply.github.com>
2024-08-31 07:24:03 +0530

    add  styles to product details page

--- src/screens/ProductDetails.js
+++ src/screens/ProductDetails.js
@@ -1,9 +1,9 @@
-import { View, Text, Image, Pressable, ScrollView } from "react-native";
+import { View, Text, Image, ScrollView,Pressable } from "react-native";
 import { useEffect, useState } from "react";
 import "nativewind";
 
-const ProductDetails = ({items}) => {
-  const [products, setProducts] = useState('');
+const ProductDetails = ({ items }) => {
+  const [products, setProducts] = useState({ sizes: [] });
 
   useEffect(() => {
     getAllProducts();
@@ -18,27 +18,46 @@ const ProductDetails = ({items}) => {
       const randomIndex = Math.floor(Math.random() * data.data.length);
       const randomProduct = data.data[randomIndex];
       setProducts(randomProduct);
-      
     } catch (error) {
       console.error("Error fetching products:", error);
     }
   };
-  
+
   return (
-    <View >
+    <View>
       <ScrollView>
         <Image
           className="w-full aspect-square"
           source={{ uri: products.mainImage }}
         />
-        <Text> {products.name}</Text>
-      </ScrollView>
+        <Text className="text-4xl  font-medium my-2 p-4">{products.name}</Text>
+        <Text className="text-2xl p-4 font-semibold">
+          Color: {products.colour}
+        </Text>
+        {Array.isArray(products.sizes) && (
+          <View className="flex-row flex-wrap my-4">
+            {products.sizes.map((size, index) => (
+              <Text key={index} className="font-bold  mr-2 p-4">
+                Size: {size}
+              </Text>
+            ))}
+          </View>
+        )}
+        <Text className="text-2xl p-4 font-bold">
+          Price: {products.price?.amount} {products.price?.currency}
+        </Text>
+        <Text className="font-semibold text-base my-2.5 leading-[30px] p-4">
+          {products.description}
+        </Text>
+        <View className="py-10">
 
+        </View>
+      </ScrollView>
       <Pressable
-        className="absolute bottom-0 w-[90%] bg-black 
+        className="absolute bottom-8 w-[90%] bg-black 
                     self-center p-5 rounded-full items-center"
       >
-        <Text className="text-white font-medium text-base">Add to cart</Text>
+        <Text className="text-white font-medium text-lg">Add to cart</Text>
       </Pressable>
     </View>
   );
